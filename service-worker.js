34c96c49d95cfb61e710ebdc694b5f78262dc644
ezrakwiimba <152643832+ezrakwiimba@users.noreply.github.com>
2023-12-01 21:28:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('your-cache-name').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                // Add other files you want to cache
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
 
